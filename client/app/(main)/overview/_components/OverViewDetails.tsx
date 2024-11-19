cc669131c4d75b65e758fd17956bf5592ac979ff
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2024-11-19 09:56:51 +0600

    Issue : not showing overview when no transaction is here is resolved

--- client/app/(main)/overview/_components/OverViewDetails.tsx
+++ client/app/(main)/overview/_components/OverViewDetails.tsx
@@ -15,38 +15,35 @@ export const OverviewDetails = () => {
 
   return (
     <section>
-      {overviewData ? (
-        <>
-          <div className='flex items-center justify-between'>
-            <h1 className='text-lg font-semibold'>
-              Date : {format(date, 'PPP')}
-            </h1>
-            <DatePicker date={date || new Date()} onDateSelect={onDateChange} />
-          </div>
-          <div className='mb-2 mt-6 grid gap-4 md:grid-cols-3 xl:grid-cols-5'>
-            <OverviewCard label='Balance' value={overviewData.balance} />
-            <OverviewCard label='Revenue' value={overviewData.revenue} />
-            <OverviewCard label='Collection' value={overviewData.collection} />
-            <OverviewCard label='Due' value={overviewData.due} />
-            <OverviewCard
-              label='Commission Paid'
-              value={overviewData.commission}
-            />
-            <OverviewCard
-              label='Commission To Be Paid'
-              value={overviewData.commissionToBePaid}
-            />
-            <OverviewCard
-              label='Utility Expense'
-              value={overviewData.utilityExpense}
-            />
-          </div>
-          {overviewData.bills && (
-            <BillTable label='All Bills' bills={overviewData.bills} />
-          )}
-        </>
+      <div className='flex items-center justify-between'>
+        <h1 className='text-lg font-semibold'>Date : {format(date, 'PPP')}</h1>
+        <DatePicker date={date || new Date()} onDateSelect={onDateChange} />
+      </div>
+      <div className='mb-2 mt-6 grid gap-4 md:grid-cols-3 xl:grid-cols-5'>
+        <OverviewCard label='Balance' value={overviewData?.balance || 0} />
+        <OverviewCard label='Revenue' value={overviewData?.revenue || 0} />
+        <OverviewCard
+          label='Collection'
+          value={overviewData?.collection || 0}
+        />
+        <OverviewCard label='Due' value={overviewData?.due || 0} />
+        <OverviewCard
+          label='Commission Paid'
+          value={overviewData?.commission || 0}
+        />
+        <OverviewCard
+          label='Commission To Be Paid'
+          value={overviewData?.commissionToBePaid || 0}
+        />
+        <OverviewCard
+          label='Utility Expense'
+          value={overviewData?.utilityExpense || 0}
+        />
+      </div>
+      {overviewData?.bills && overviewData.bills.length > 0 ? (
+        <BillTable label='All Bills' bills={overviewData.bills} />
       ) : (
-        <p className='mt-6 text-center font-semibold'>No Details Found</p>
+        <p className='mt-8 text-center font-semibold'>No Bill Found</p>
       )}
     </section>
   );
